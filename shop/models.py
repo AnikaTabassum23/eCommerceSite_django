1c2e43b37b9820ea833420340c191f223d9f6d0f
Anika Tabassum <anikaakhi14@gmail.com>
2023-08-29 17:42:30 +0600

    add contact

--- shop/models.py
+++ shop/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Product(models.Model):
     product_id=models.AutoField
     product_name=models.CharField(max_length=50)
@@ -12,4 +13,13 @@ class Product(models.Model):
     
     def __str__(self):
         return self.product_name
-   
+class Contact(models.Model):
+    msg_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    email = models.CharField(max_length=70, default="")
+    phone = models.CharField(max_length=70, default="")
+    desc = models.CharField(max_length=500, default="")
+
+
+    def __str__(self):
+        return self.name
